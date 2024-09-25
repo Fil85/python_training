1dc798f2536feba8d6dd2c795672a53287973a96
KLMN\Filimonov <filimonov@ascon.ru>
2024-09-25 17:26:23 +0300

    Рефакторинг кода, добавление теста на создание пустого контакта

--- test_add_group.py
+++ test_add_group.py
@@ -38,7 +38,7 @@ class TestAddGroup(unittest.TestCase):
     def create_group(self, wd, group):
         # init group creation
         wd.find_element_by_name("new").click()
-        # fill group firm
+        # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
