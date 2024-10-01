b250d1b1758b071fcdb82119efc16919ca0c4e3c
Филимонов Евгений <evgenfil85@gmail.com>
2024-10-01 22:00:05 +0300

    Выделен класс для управления сессиями

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -11,17 +11,17 @@ def app(request):
     return fixture
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Contact(firstname="Ivan", lastname="Ivanov", company="GazProm",
                                    address="Saint Petersburg", home_phone="111111", mobile_phone="222222",
                                    work_phone="333333", email="ivan@mail.ru", homepage="ivan.ru",
                                    bday="14", bmonth="May", byear="1988"))
-    app.logout()
+    app.session.logout()
 
 def test_add_empty_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Contact(firstname="", lastname="", company="",
                                address="", home_phone="", mobile_phone="",
                                work_phone="", email="", homepage="",
                                bday="10", bmonth="April", byear="1990"))
-    app.logout()
+    app.session.logout()
