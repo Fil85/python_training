4e63dc46a450e06a9c65014bf8cd4ca5ae6d6a9a
KLMN\Filimonov <filimonov@ascon.ru>
2024-10-02 17:09:21 +0300

    Создание общей фикстуры для всех тестов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,22 +1,15 @@
 # -*- coding: utf-8 -*-
-import pytest
 from model.contact import Contact
-from fixture.application import Application
 
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="Ivan", lastname="Ivanov", company="GazProm",
-                                   address="Saint Petersburg", home_phone="111111", mobile_phone="222222",
-                                   work_phone="333333", email="ivan@mail.ru", homepage="ivan.ru",
-                                   bday="14", bmonth="May", byear="1988"))
+                               address="Saint Petersburg", home_phone="111111", mobile_phone="222222",
+                               work_phone="333333", email="ivan@mail.ru", homepage="ivan.ru",
+                               bday="14", bmonth="May", byear="1988"))
     app.session.logout()
+
 
 def test_add_empty_contact(app):
     app.session.login(username="admin", password="secret")
