eda2773073a2cab2a3c239936dd5b840e04fb44f
Филимонов Евгений <evgenfil85@gmail.com>
2024-10-03 20:59:23 +0300

    Тест на редактирование контакта

---
+++ test/test_edit_contact.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from model.contact import Contact
+
+
+def test_add_group(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.edit(Contact(firstname="Dima", lastname="Filimonov", company="Dom",
+                               address="Ryazan", home_phone="001", mobile_phone="002",
+                               work_phone="003", email="dima@mail.ru", homepage="dima.ru",
+                               bday="3", bmonth="October", byear="2020"))
+    app.session.logout()
