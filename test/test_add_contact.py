c2dc118acbde23d6925edaceb38398ac617621a7
Филимонов Евгений <evgenfil85@gmail.com>
2024-10-07 20:44:11 +0300

    Оптимизация методов логин/логаут, валидация фикстуры перед каждым тестом

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,18 +3,14 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="Ivan", lastname="Ivanov", company="GazProm",
                                address="Saint Petersburg", home_phone="111111", mobile_phone="222222",
                                work_phone="333333", email="ivan@mail.ru", homepage="ivan.ru",
                                bday="14", bmonth="May", byear="1988"))
-    app.session.logout()
 
 
 def test_add_empty_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="", lastname="", company="",
                                address="", home_phone="", mobile_phone="",
                                work_phone="", email="", homepage="",
                                bday="10", bmonth="April", byear="1990"))
-    app.session.logout()
--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -3,9 +3,7 @@ from model.contact import Contact
 
 
 def test_edit_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.edit(Contact(firstname="Dima", lastname="Filimonov", company="Dom",
                                address="Ryazan", home_phone="001", mobile_phone="002",
                                work_phone="003", email="dima@mail.ru", homepage="dima.ru",
                                bday="3", bmonth="October", byear="2020"))
-    app.session.logout()
