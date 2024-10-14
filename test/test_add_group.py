70de638ee27843887fc1a1bd60c2289ca9040a80
KLMN\Filimonov <filimonov@ascon.ru>
2024-10-14 10:39:04 +0300

    Сортировка списков перед сравнением (группы)

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,19 +2,22 @@
 from model.group import Group
 
 
+
 def test_add_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="test2", header="tyfnej", footer="test123"))
+    group = Group(name="test2", header="tyfnej", footer="test123")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
-    old_groups.append(new_groups[-1])
-    assert old_groups == new_groups
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def test_add_empty_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="", header="", footer=""))
+    group = Group(name="test2", header="tyfnej", footer="test123")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
-    old_groups.append(new_groups[-1])
-    assert old_groups == new_groups
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -3,22 +3,26 @@ from model.group import Group
 
 
 def test_edit_group_name(app):
-    old_groups = app.group.get_group_list()
+    group = Group(name="test")
     if app.group.count() == 0:
-        app.group.create(Group(name="test"))
-    app.group.edit_first_group(Group(name="New group"))
+        app.group.create(group)
+    old_groups = app.group.get_group_list()
+    group.id = old_groups[0].id
+    app.group.edit_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
-    old_groups[0] = new_groups[0]
-    assert old_groups == new_groups
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def test_edit_group_header(app):
-    old_groups = app.group.get_group_list()
+    group = Group(name="test")
     if app.group.count() == 0:
-        app.group.create(Group(header="test"))
-    app.group.edit_first_group(Group(header="New header"))
+        app.group.create(group)
+    old_groups = app.group.get_group_list()
+    group.id = old_groups[0].id
+    app.group.edit_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
-    old_groups[0] = new_groups[0]
-    assert old_groups == new_groups
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
