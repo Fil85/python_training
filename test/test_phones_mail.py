285db53de1ed6e1fbb5855b9aafc2f3e7d5d9cf9
KLMN\Filimonov <filimonov@ascon.ru>
2024-10-21 14:23:49 +0300

    Добавление случайностей в тесты сравнения свойств (имя, фамилия, адрес, телефон, почта).

--- test/test_phones_mail.py
+++ test/test_phones_mail.py
@@ -1,4 +1,5 @@
 import re
+from random import randrange
 
 
 def test_phones_on_home_page(app):
@@ -16,8 +17,10 @@ def test_phones_on_contact_view_page(app):
 
 
 def test_all_data_on_home_page(app):
-    contact_from_home_page = app.contact.get_contact_list()[0]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    old_contacts = app.contact.get_contact_list()
+    index = randrange(len(old_contacts))
+    contact_from_home_page = app.contact.get_contact_list()[index]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
     assert contact_from_home_page.firstname == contact_from_edit_page.firstname
     assert contact_from_home_page.lastname == contact_from_edit_page.lastname
     assert contact_from_home_page.address == contact_from_edit_page.address
