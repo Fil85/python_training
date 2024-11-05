c258b5975cce25140cfb2ccb397a7be8386cd658
Филимонов Евгений <evgenfil85@gmail.com>
2024-11-05 21:15:59 +0300

    Сравнение данных загруженных из БД для контактов (удаление контакта).

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -9,7 +9,7 @@ def test_delete_some_contact(app, db, check_ui):
     old_contacts = db.get_contact_list()
     contact = random.choice(old_contacts)
     app.contact.delete_contact_by_id(contact.id)
-    new_contacts = db.get_group_list()
+    new_contacts = db.get_contact_list()
     assert len(old_contacts) - 1 == len(new_contacts)
     old_contacts.remove(contact)
     assert old_contacts == new_contacts
