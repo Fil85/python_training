1d7bf6aec6a99fbb34bed8f4771181929f95e85a
KLMN\Filimonov <filimonov@ascon.ru>
2024-11-13 12:41:36 +0300

    Исправление тестов на удаление/добавление контактов из группы.

--- test/test_contact_in_groups.py
+++ test/test_contact_in_groups.py
@@ -5,16 +5,21 @@ import random
 
 
 def test_add_contact_to_group(app, db):
+    contact_id = ''
     if len(db.get_contact_list()) == 0:
         app.contact.create(Contact(firstname="John"))
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name="test"))
-    contacts = db.get_contact_list()
+    if len(db.get_contact_list()) == len(db.get_contact_list_in_all_groups()):
+        app.contact.create(Contact(firstname="John"))
+    for contact in db.get_contact_list():
+        if contact.id not in db.get_contact_list_in_all_groups():
+            contact_id = contact.id
+            break
     groups = db.get_group_list()
-    contact = random.choice(contacts)
     group = random.choice(groups)
-    app.contact.add_contact_to_group(contact.id, group.id)
-    assert contact.id in db.get_contact_list_in_our_group(group)
+    app.contact.add_contact_to_group(contact_id, group.id)
+    assert contact_id in db.get_contact_list_in_our_group(group)
 
 
 def test_del_contact_from_group(app, db):
@@ -25,7 +30,11 @@ def test_del_contact_from_group(app, db):
     groups = db.get_group_list()
     group = random.choice(groups)
     contacts = db.get_contact_list_in_our_group(group)
-    if len(contacts) == 0:
+    if len(db.get_contact_list_in_all_groups()) != 0:
+        while len(contacts) == 0:
+            group = random.choice(groups)
+            contacts = db.get_contact_list_in_our_group(group)
+    else:
         contacts_all = db.get_contact_list()
         contact = random.choice(contacts_all)
         app.contact.add_contact_to_group(contact.id, group.id)
